aa7c71268ce6326560b1692fd269389a68a8f796
tatamingi <tatamingi@gmail.com>
2019-11-18 20:33:11 +0300

    deep unit tests

---
+++ src/app/heroes/heroes.component.deep.spec.ts
@@ -0,0 +1,48 @@
+import { ComponentFixture, TestBed } from "@angular/core/testing";
+import { HeroesComponent } from "./heroes.component";
+import { HeroService } from "../hero.service";
+import { NO_ERRORS_SCHEMA } from "@angular/core";
+import { of } from "rxjs";
+import { HeroComponent } from "../hero/hero.component";
+import { By } from "@angular/platform-browser";
+
+describe('HeroesComponent (deep tests)', () => {
+  let fixture: ComponentFixture<HeroesComponent>;
+  let mockHeroService;
+  let HEROES;
+
+  beforeEach(() => {
+    HEROES = [
+      {id: 1, name: 'SpiderDude', strength: 8},
+      {id: 2, name: 'Wonderful Woman', strength: 24},
+      {id: 3, name: 'SuperDude', strength: 55}
+    ]
+
+    mockHeroService = jasmine.createSpyObj(['getHeroes', 'addHero', 'deleteHero']);
+
+    TestBed.configureTestingModule({
+      declarations: [
+        HeroesComponent,
+        HeroComponent
+      ],
+      providers: [
+        { provide: HeroService, useValue: mockHeroService } // все еще используем мок сервиса
+      ],
+      schemas: [ NO_ERRORS_SCHEMA ]
+    })
+    fixture = TestBed.createComponent(HeroesComponent);
+  })
+
+  it('should render each hero as a HeroComponent', () => {
+    mockHeroService.getHeroes.and.returnValue(of(HEROES));
+
+    fixture.detectChanges(); // run ngOnInit
+
+    const heroComponentDEs = fixture.debugElement.queryAll(By.directive(HeroComponent));
+    expect(heroComponentDEs.length).toEqual(3);
+
+    heroComponentDEs
+      .forEach((heroComponentDE, index) =>
+        expect(heroComponentDE.componentInstance.hero).toEqual(HEROES[index]))
+  })
+})
