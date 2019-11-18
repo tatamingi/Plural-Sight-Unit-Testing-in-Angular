fb2cc9daa3292895d08ef500175d476e7d397b55
tatamingi <tatamingi@gmail.com>
2019-11-18 20:32:11 +0300

    shallow integration tests

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -10,7 +10,7 @@ import { HeroService } from '../hero.service';
 })
 export class HeroesComponent implements OnInit {
   heroes: Hero[];
-fbf
+
   constructor(private heroService: HeroService) { }
 
   ngOnInit() {
